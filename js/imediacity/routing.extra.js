724b8881bf1411625e824188a3fc8ec4cd6ce8a2
m.dantonio@cineca.it <m.dantonio@cineca.it>
2016-09-19 07:24:03 +0200

    Added route for a search page

--- js/imediacity/routing.extra.js
+++ js/imediacity/routing.extra.js
@@ -1,43 +1,20 @@
 (function() {
 'use strict';
 
+loggedLandingPage = "logged.search";
+
 angular.module('web').constant('customRoutes',
  {
 // JUST A TEST
 // Note: this will automatically check api online as not subchild of 'logged'
-    'test': {
-        url: "/test",
+    'logged.search': {
+        url: "/search",
         views: {
-            "main": {
+            "loggedview@logged": {
                 dir: 'blueprint',
                 // OR
                 //dir: 'base',
-                templateUrl: 'test.html',
-            }
-        }
-    },
-///////////////
-// LOGGED TEST
-// Note: this will automatically check auth since is subchild of 'logged'
-    'logged.test': {
-        url: "/yes",
-        views: {
-            "loggedview": {
-                dir: 'blueprint',
-                templateUrl: 'test.html',
-            }
-        }
-    },
-    'logged.test.sub': {
-        url: "/:id",
-        views: {
-
-// ADD SUBVIEW IN PARENT AND USE IT
-            "sub": {
-// OVERWRITING A PARENT VIEW
-            //"loggedview@logged": {
-                dir: 'blueprint',
-                templateUrl: 'sub.html',
+                templateUrl: 'search.html',
             }
         }
     }
