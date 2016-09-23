72f281a6d66e66b86e045858e1f2252607b84e24
m.dantonio@cineca.it <m.dantonio@cineca.it>
2016-09-23 15:56:30 +0200

    Bug fix

--- js/graphbased/app.js
+++ js/graphbased/app.js
@@ -1,52 +1,16 @@
 (function() {
   'use strict';
 
-/* Define another module?
-console.log('PRE');
-angular.module('blueprint', ['ui.router']);
-console.log('POST');
-*/
-
   angular
     .module('web', [
-// BASE
-        //'ngAnimate',
-        'ngSanitize',
-// ROUTING
-        'ui.router', //'ngRoute',
-// BOOTSTRAP
-        //'mgcrea.ngStrap',
-// OFFICIAL MATERIAL
-        'ngMaterial',
-// MATERIAL DESIGN
-        'ui.materialize',
-// AUTH
-        'satellizer',
-// KEYBOARD SHORTCUTS
-        'cfp.hotkeys',
-// FORMS FROM JSON SCHEMAS
-        'formly',
-        'formlyBootstrap',
-//////////////////////////////////////
-/* Custom */
-
-// THE LOADING BAR
-        //'angular-loading-bar',
-        //'cfp.loadingBar',
-// IMAGES LAZ LOAD
-        // 'angularLazyImg',
-// MAKE DATA TREE explorer
-        // 'treeControl',
-// DRAG AND DROP
-        // 'angular-sortable-view',
-// EDITABLE FORMS
-        // 'xeditable',
-// RESTARTABLE UPLOAD
-        // 'flow',
-
-/* Custom end */
-//////////////////////////////////////
-
+        // 'ngSanitize',
+        // 'ui.router',
+        // 'ngMaterial',
+        // 'ui.materialize',
+        // 'satellizer',
+        // 'cfp.hotkeys',
+        // 'formly',
+        // 'formlyBootstrap',
     ]);
 
 })();
