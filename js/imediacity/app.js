70de0043a406ea2580f8738e1ffcf6aba5c403fa
m.dantonio@cineca.it <m.dantonio@cineca.it>
2016-09-27 07:41:55 +0200

    Bootstrapping

--- js/imediacity/app.js
+++ js/imediacity/app.js
@@ -9,44 +9,13 @@ console.log('POST');
 
   angular
     .module('web', [
-// BASE
-        //'ngAnimate',
         'ngSanitize',
-// ROUTING
-        'ui.router', //'ngRoute',
-// BOOTSTRAP
-        //'mgcrea.ngStrap',
-// OFFICIAL MATERIAL
-        'ngMaterial',
-// MATERIAL DESIGN
-        'ui.materialize',
-// AUTH
+        'ui.router',
+        'ui.bootstrap',
         'satellizer',
-// KEYBOARD SHORTCUTS
         'cfp.hotkeys',
-// FORMS FROM JSON SCHEMAS
         'formly',
-        'formlyBootstrap',
-//////////////////////////////////////
-/* Custom */
-
-// THE LOADING BAR
-        //'angular-loading-bar',
-        //'cfp.loadingBar',
-// IMAGES LAZ LOAD
-        // 'angularLazyImg',
-// MAKE DATA TREE explorer
-        // 'treeControl',
-// DRAG AND DROP
-        // 'angular-sortable-view',
-// EDITABLE FORMS
-        // 'xeditable',
-// RESTARTABLE UPLOAD
-        // 'flow',
-
-/* Custom end */
-//////////////////////////////////////
-
+        'formlyBootstrap'
     ]);
 
 })();
