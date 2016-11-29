df50a5e325b7cab9cb2fdb822d75981607734e87
Mattia D'Antonio <m.dantonio@cineca.it>
2016-11-29 08:47:29 +0100

    Added authentication for upload, preliminary code to save uploaded files

--- apis/upload.py
+++ apis/upload.py
@@ -7,7 +7,7 @@ An endpoint example
 from commons.logs import get_logger
 from ..base import ExtendedApiResource
 from .. import decorators as decorate
-# from ...auth import authentication
+from ...auth import authentication
 # from flask_restful import request
 # from commons import htmlcodes as hcodes
 # from commons.services.uuid import getUUID
@@ -18,8 +18,23 @@ logger = get_logger(__name__)
 #####################################
 class Upload(ExtendedApiResource):
 
-    # @authentication.authorization_required
+    @authentication.authorization_required
+    @decorate.add_endpoint_parameter('file')
+    # @decorate.add_endpoint_parameter('flowChunkSize')
+    # @decorate.add_endpoint_parameter('flowCurrentChunkSize')
+    # @decorate.add_endpoint_parameter('flowTotalSize')
+    # @decorate.add_endpoint_parameter('flowIdentifier')
+    @decorate.add_endpoint_parameter('flowFilename')
+    # @decorate.add_endpoint_parameter('flowRelativePath')
+    # @decorate.add_endpoint_parameter('flowTotalChunks')
     @decorate.apimethod
-    def get(self):
+    def post(self):
+
+        data = self.get_input()
+        logger.critical(data)
+        # params = self.get_input()
+        filename = self.get_input(single_parameter='flowFilename')
+        with open(filename, "a") as f:
+            f.write(".")
 
         return self.force_response("received")
--- js/imediacity/upload.controller.js
+++ js/imediacity/upload.controller.js
@@ -3,10 +3,17 @@
 
 angular.module('web').controller('UploadController', UploadController);
 
-function UploadController($scope, $log, DataService, noty)
+function UploadController($scope, $log, $auth, noty)
 {
 	var self = this;
 
+	$scope.flowOptions = {
+        target: apiUrl + '/upload',
+        testChunks: false,
+        permanentErrors: [ 401, 405, 500, 501 ],
+        // withCredentials: true,
+        headers: {Authorization : 'Bearer ' + $auth.getToken()}
+    }
 }
 
 })();
