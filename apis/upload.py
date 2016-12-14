995d1c72ffe8b5c933fb6e1ab22f48c51325c1cf
Mattia D'Antonio <m.dantonio@cineca.it>
2016-12-14 08:53:45 +0100

    Using the upload function implemented in backend

--- apis/upload.py
+++ apis/upload.py
@@ -11,24 +11,20 @@ from .. import decorators as decorate
 from ...auth import authentication
 from commons import htmlcodes as hcodes
 from flask_restful import request
-from werkzeug import secure_filename
+from ..services.uploader import Uploader
 # from commons.services.uuid import getUUID
 
 logger = get_logger(__name__)
 
 
 #####################################
-class Upload(ExtendedApiResource):
+class Upload(Uploader, ExtendedApiResource):
 
     @authentication.authorization_required
-    # @decorate.add_endpoint_parameter('flowChunkSize')
-    # @decorate.add_endpoint_parameter('flowCurrentChunkSize')
-    # @decorate.add_endpoint_parameter('flowTotalSize')
-    # @decorate.add_endpoint_parameter('flowIdentifier')
     @decorate.add_endpoint_parameter('flowFilename')
-    # @decorate.add_endpoint_parameter('flowRelativePath')
     @decorate.add_endpoint_parameter('flowChunkNumber')
     @decorate.add_endpoint_parameter('flowTotalChunks')
+    @decorate.add_endpoint_parameter('flowChunkSize')
     @decorate.apimethod
     def post(self):
 
@@ -39,29 +35,26 @@ class Upload(ExtendedApiResource):
         except self.graph.User.DoesNotExist:
             user_node = None
 
-        # params = self.get_input()
-        chunk_number = self.get_input(single_parameter='flowChunkNumber')
-        chunk_total = self.get_input(single_parameter='flowTotalChunks')
-        filename = self.get_input(single_parameter='flowFilename')
-        sec_filename = secure_filename(filename)
         root_dir = os.path.join("/uploads", user_node.uuid)
-        abs_fname = os.path.join(root_dir, sec_filename)
-
         if not os.path.exists(root_dir):
             os.mkdir(root_dir)
 
-        if chunk_number == "1":
-            if os.path.exists(abs_fname):
-                os.remove(abs_fname)
+        chunk_number = int(self.get_input(single_parameter='flowChunkNumber'))
+        chunk_total = int(self.get_input(single_parameter='flowTotalChunks'))
+        chunk_size = int(self.get_input(single_parameter='flowChunkSize'))
+        filename = self.get_input(single_parameter='flowFilename')
 
-        with open(abs_fname, "ab") as f:
-            request.files['file'].save(f)
-            f.close()
+        abs_fname = self.ngflow_upload(
+            filename, root_dir, request.files['file'],
+            chunk_number, chunk_size, chunk_total,
+            overwrite=True
+        )
 
+# TO FIX: what happens if last chunk doesn't arrive as last?
         if chunk_number == chunk_total:
 
             properties = {}
-            properties["filename"] = sec_filename
+            properties["filename"] = abs_fname
             properties["title"] = filename
             video = self.graph.createNode(self.graph.Video, properties)
             if user_node is not None:
