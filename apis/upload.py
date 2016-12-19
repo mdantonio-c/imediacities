92c75e5b078c91202ba3543d3655009cee59821c
Mattia D'Antonio <m.dantonio@cineca.it>
2016-12-19 12:17:03 +0100

    Upload dir is not based on group instead of user

--- apis/upload.py
+++ apis/upload.py
@@ -13,7 +13,7 @@ from flask_restful import request
 from ..services.uploader import Uploader
 from ..services.neo4j.graph_endpoints import GraphBaseOperations
 # from ..services.neo4j.graph_endpoints import myGraphError
-# from ..services.neo4j.graph_endpoints import returnError
+from ..services.neo4j.graph_endpoints import returnError
 from ..services.neo4j.graph_endpoints import graph_transactions
 from ..services.neo4j.graph_endpoints import catch_graph_exceptions
 # from commons.services.uuid import getUUID
@@ -38,7 +38,16 @@ class Upload(Uploader, GraphBaseOperations):
 
         self.initGraph()
 
-        root_dir = os.path.join("/uploads", self._current_user.uuid)
+        group = self.getSingleLinkedNode(self._current_user.belongs_to)
+
+        if group is None:
+            return returnError(
+                self,
+                label="Invalid request",
+                error="No group defined for this user",
+                code=hcodes.HTTP_BAD_REQUEST)
+
+        root_dir = os.path.join("/uploads", group.uuid)
         if not os.path.exists(root_dir):
             os.mkdir(root_dir)
 
