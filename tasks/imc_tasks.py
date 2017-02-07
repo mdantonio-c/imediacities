389b8caacee6f74f6fcb176d759a1ff6d1c2cc62
Mattia D'Antonio <m.dantonio@cineca.it>
2017-02-07 12:08:50 +0100

    Basic structure for celery tasks

---
+++ tasks/imc_tasks.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from ...services.celery import celery_app
+from commons.logs import get_logger
+import os
+
+log = get_logger(__name__)
+
+
+####################
+# Define your celery tasks
+
+def progress(self, state, info):
+    if info is not None:
+        log.debug("%s [%s]" % (state, info))
+    self.update_state(state=state)
+
+
+@celery_app.task(bind=True)
+def import_file(self, path):
+    with celery_app.app.app_context():
+
+        progress(self, 'Starting import', path)
+
+        # self.graph = celery_app.get_service('neo4j')
+
+        try:
+
+            # self.fileNode = self.graph.File.nodes.get(accession=accession)
+
+            # METADATA EXTRACTION
+            filename, file_extension = os.path.splitext(path)
+
+            if file_extension.startswith("."):
+                file_extension = file_extension[1:]
+
+            progress(self, 'Extracting metadata', path)
+            with open(path, 'r') as f:
+                while True:
+                    line = f.readline()
+                    log.info(line)
+
+            # SAVE METADATA
+            # self.fileNode.status = 'completed'
+            # self.fileNode.metadata = metadata
+            # self.fileNode.save()
+
+            progress(self, 'Completed', path)
+
+        except self.graph.File.DoesNotExist:
+            progress(self, 'Import error', None)
+            log.error("Task error, path %s not found" % path)
+
+        return 1
+
