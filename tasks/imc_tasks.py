68b1abf956638dfdafb13f8bd95b9f055c6a6844
Mattia D'Antonio <m.dantonio@cineca.it>
2017-04-10 12:40:30 +0200

    Built the structure of the import task

--- tasks/imc_tasks.py
+++ tasks/imc_tasks.py
@@ -28,10 +28,12 @@ def import_file(self, path, resource_id):
 
         self.graph = celery_app.get_service('neo4j')
 
-        resource = None
+        xml_resource = None
         try:
 
-            resource = self.graph.Stage.nodes.get(uuid=resource_id)
+            xml_resource = self.graph.Stage.nodes.get(uuid=resource_id)
+
+            group = self.getSingleLinkedNode(xml_resource.ownership)
 
             # METADATA EXTRACTION
             filename, file_extension = os.path.splitext(path)
@@ -39,39 +41,84 @@ def import_file(self, path, resource_id):
             if file_extension.startswith("."):
                 file_extension = file_extension[1:]
 
-            video_filename = extract_descriptive_metadata(self, path)
-            log.info("Video filename %s", video_filename)
-            if (video_filename is None):
-                raise Exception("No video match found importing file " + path)
-
-            log.debug('filename [%s], extension [%s]', filename, file_extension)
             progress(self, 'Extracting descriptive metadata', path)
-            
-            # EXECUTE AUTOMATC TOOLS
-            progress(self, 'Executing automatic tools', path)
-            params = []
-            params.append("/imedia-pipeline-cin/analyze.py")
-            params.append(video_filename)
-            # bash = BashCommands()
-            # try:
-            #     output = bash.execute_command(
-            #         "python3",
-            #         params,
-            #         parseException=True
-            #     )
-            #     log.info(output)
 
-            # except BaseException as e:
-            #     log.error(e)
-            #     raise(e)
+            videos = extract_descriptive_metadata(self, path)
+            if (videos is None):
+                raise Exception(
+                    "No video match found importing file %s" % path)
 
-            # SAVE AUTOMATIC ANNOTATIONS
-            progress(self, 'Extracting automatic annotations', path)
-            # TODO
+            for video_filename in videos:
 
-            resource.status = 'COMPLETED'
-            resource.status_message = 'Nothing to declare'
-            resource.save()
+                if video_filename is None:
+                    log.warning("Why the video filename is None??")
+                    continue
+
+                log.info("Video filename %s", video_filename)
+
+                # TO FIX: will be something like: root + video_filename ?
+                # root = basedir(path) ?
+                video_path = video_filename
+
+                log.debug('filename [%s], extension [%s]' % (filename, file_extension))
+
+                # Creating video resource
+                properties = {}
+                properties['filename'] = video_filename
+                properties['path'] = video_path
+
+                try:
+                    # This is a task restart? What to do in this case?
+                    video_node = self.graph.Stage.nodes.get(**properties)
+                    log.debug("Video resource already exist for %s" % video_path)
+                except self.graph.Stage.DoesNotExist:
+                    video_node = self.graph.Stage(**properties).save()
+                    video_node.ownership.connect(group)
+                    log.debug("Video resource created for %s" % video_path)
+
+                video_node.status = "IMPORTING"
+                # TO FIX: video analysis will be a new tasks
+                # task = analyze_video.apply_async(
+                #     args=[path, video_node.uuid],
+                #     countdown=20
+                # )
+                # video_node.task_id = task.id
+                video_node.save()
+
+                # EXECUTE AUTOMATC TOOLS
+                progress(self, 'Executing automatic tools', path)
+                params = []
+                params.append("/imedia-pipeline-cin/analyze.py")
+                params.append(video_filename)
+                bash = BashCommands()
+                try:
+                    output = bash.execute_command(
+                        "python3",
+                        params,
+                        parseException=True
+                    )
+                    log.info(output)
+
+                except BaseException as e:
+                    log.error(e)
+                    video_node.status = 'ERROR'
+                    video_node.status_message = str(e)
+                    video_node.save()
+                    raise(e)
+
+                # SAVE AUTOMATIC ANNOTATIONS
+                progress(self, 'Extracting automatic annotations', path)
+                # TODO
+                video_filename.status = 'COMPLETED'
+                video_filename.status_message = 'Nothing to declare'
+                video_filename.save()
+
+                # TO FIX: move video in the datadir
+                # os.rename(video_path, datadir/video_node.uuid)
+
+            xml_resource.status = 'COMPLETED'
+            xml_resource.status_message = 'Nothing to declare'
+            xml_resource.save()
 
             progress(self, 'Completed', path)
             # os.remove(path)
@@ -82,17 +129,17 @@ def import_file(self, path, resource_id):
         except Exception as e:
             progress(self, 'Import error', None)
             log.error("Task error, %s" % e)
-            if resource is not None:
-                resource.status = 'ERROR'
-                resource.status_message = str(e)
-                resource.save()
-            #raise e/media/gtrotta/OS/Users/giuseppe/Desktop/progetti/I-MEDIA-CITIES/WP6 - Metadata Modelling/EFG/xsd/efg_3.2.07.xsd
+            if xml_resource is not None:
+                xml_resource.status = 'ERROR'
+                xml_resource.status_message = str(e)
+                xml_resource.save()
+            raise e
 
         return 1
 
 
 def extract_descriptive_metadata(self, path):
-    """ 
+    """
     Simple metadata ingestion:
       - validate against EFG-XSD schema
       - look for the record with the filename in ref elem
@@ -113,15 +160,21 @@ def extract_descriptive_metadata(self, path):
     #             break
     #         #log.info(line.strip())
 
-    #e = xml.etree.ElementTree.parse(path).getroot()
+    # e = xml.etree.ElementTree.parse(path).getroot()
     parser = EFG_XMLParser()
     root = parser.getRoot(path)
-    #tree = etree.parse(path)
+    # tree = etree.parse(path)
     log.debug(ET.tostring(root, encoding='UTF-8'))
+
+    # This will be a list of video extracted from XML file
+    # return root
+    videos = []
+    videos.append(root)
+    return videos
 
 
 def extract_automatic_annotation(self, path):
     """ """
-    #tree = etree.parse(path)
-    #log.debug(etree.tostring(tree, pretty_print=True))
+    # tree = etree.parse(path)
+    # log.debug(etree.tostring(tree, pretty_print=True))
     pass
