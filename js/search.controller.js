90dfc3773cba3f53b2824a7c8b338c7c149b7a05
Michele Carpene <m.carpen@cineca.it>
2017-05-23 12:43:06 +0200

    minor fixes, added shots event handler

--- js/search.controller.js
+++ js/search.controller.js
@@ -30,22 +30,19 @@ app.filter('searchFor', function() {
 
 app.directive('scrollOnClick', function() {
   return {
-    restrict: 'A',
-    link: function($scope, $elm) {
-      $elm.on('click', function() {
-        $(".scrollmenu").animate({scrollLeft: $elm.offset().left}, "slow");
-      });
-    }
-  }
-});
-
-app.directive('scrollOnClick', function() {
-  return {
     //restrict: 'A',
     link: function($scope, $elm) {
       $elm.on('click', function() {
       	//alert($elm[0].parentNode.className);
         $(".scrollmenu").animate({scrollLeft: $elm.offset().left}, "slow");
+        // play video from selected shot
+		var duration = $elm[0].firstElementChild.attributes.duration.value;
+		var nshots = $elm[0].firstElementChild.attributes.nshots.value;
+		var progr = $elm[0].firstElementChild.attributes.progr.value;
+		var myVid = angular.element(window.document.querySelector('#videoarea'));
+		var currtime = (duration/nshots)*progr;
+		myVid[0].currentTime = currtime;
+		myVid[0].play();
       });
     }
   }
@@ -159,16 +156,14 @@ function SearchController($scope, $log, $document, $http, $auth, DataService, no
 			self.shots = out_data.data;
 			for (var i=0; i<self.shots.length; i++) { 
 			    var thumblink = self.shots[i].links.thumbnail;
-				$scope.items.push(thumblink); 
-			}
-		});
-
-  		DataService.getVideoShots(self.selectedVideoId).then(
-		function(out_data) {
-			self.shots = out_data.data;
-			for (var i=0; i<self.shots.length; i++) { 
-			    var thumblink = self.shots[i].links.thumbnail;
-				$scope.items.push(thumblink); 
+			    var start_frame = self.shots[i].attributes.start_frame_idx;
+			    var frameshot = [2];
+			    frameshot[0] = start_frame;
+				frameshot[1] = self.video.relationships.item[0].attributes.duration;
+				frameshot[2] = self.shots.length;
+				frameshot[3] = i;
+			    frameshot[4] = thumblink;
+				$scope.items.push(frameshot); 
 			}
 		});
 
@@ -242,95 +237,5 @@ function SearchController($scope, $log, $document, $http, $auth, DataService, no
 	};
 
 }
-
-/*function loadSampleVideos() {
-	var videos = [
-		{
-                        "id": "1",
-                        "title": "Bologna Ieri e Oggi",
-                        "duration": "433",
-                        "movieurl": "http://130.186.13.19/static/assets/videos/BolognaIeriEOggi.mp4",
-                        "moviesposter": "http://www.cinetecadibologna.it/imageserver/lightboxenlarge/files/festival/CinemaRitrovato/2012/1912/Bologna.jpg",
-                        "year": "1921",
-                        "description": "Dopo aver fatto il giro del mondo torniamo nel nostro paese, cento anni fa. ‘Un paese antico, moderno, anzi pittoresco’: nel 1911 viene fondata a Milano la società cinematografica Panorami Italici, costituita, come da statuto, per realizzare e promuovere “cinematografie delle bellezze italiane all’estero”. Nel 1912 l’Ambrosio inaugura la serie Bellezze italiche che si impegna a filmare gli scorci più suggestivi del paesaggio italiano. Le riproduzioni filmate delle meraviglie naturali, delle rovine storiche, delle città monumentali italiane sono particolarmente apprezzate dagli spettatori stranieri che possono godere di un Grand Tour virtuale a basso costo. ",
-                        "locname": "Bologna",
-                        "location": [
-                                {"lat": "44.5019603", "lng": "11.2855505"}
-                        ],
-                        "frames": [
-                                {"mainchar": "100-200"},
-                                {"outside": "300-400"}
-                        ]
-                },
-                {
-                        "id": "2",
-                        "title": "L'Italia negli anni 60",
-                        "duration": "414",
-                        "movieurl": "http://130.186.13.19/static/assets/videos/ItaliaAnni60.mp4",
-                        "moviesposter": "http://www.panoramio.com/photos/original/7143296.jpg",
-                        "year": "1960",
-                        "description": "Documentario sull'Italia negli anni del boom economico, con immagini di repertorio dell'epoca...",
-                        "locname": "Italy",
-                        "location":[
-                                {"lat": "44.7", "lng": "12.9"}
-                        ],
-                        "frames": [
-                                {"mainchar": "50-80"},
-                                {"outside": "200-300"}
-                        ]
-                }
-		/*{
-			"id": "1",
-			"title": "Happy Feet",
-			"duration": "82",
-			"movieurl": "http://html5videoformatconverter.com/data/images/happyfit2.mp4",
-			"moviesposter": "http://html5videoformatconverter.com/data/images/screen.jpg",
-			"year": "2006",
-			"description": "Into the world of the Emperor Penguins, who find their soul mates through song, a penguin is born who cannot sing. But he can tap dance something fierce!",
-			"locname": "south pole",
-			"location": [
-				{"lat": "-67.222", "lng": "78.98"}
-			],
-			"frames": [
-				{"mainchar": "10-20"},
-				{"outside": "30-40"}
-			]
-		},
-		{
-			"id": "2",
-			"title": "Sintel",
-			"duration": "52",
-			"movieurl": "http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4",
-			"moviesposter": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Sintel_movie_4K.webm/500px-seek%3D120-Sintel_movie_4K.webm.jpg",
-			"year": "2010",
-			"description": "The film follows a girl named Sintel who is searching for a baby dragon she calls Scales. A flashback reveals that Sintel found Scales with its wing injured and helped care for it, forming a close bond with it. By the time its wing recovered and it was able to fly, Scales was caught by an adult dragon.",
-			"locname": "greenland",
-			"location":[
-				{"lat": "55.3", "lng": "-45.45"}
-			],
-			"frames": [
-				{"mainchar": "5-20"},
-				{"outside": "30-40"}
-			]
-		},
-		{
-			"id": "3",
-			"title": "Big Buck Bunny",
-			"duration": "60",
-			"movieurl": "http://www.ioncannon.net/examples/vp8-webm/big_buck_bunny_480p.webm",
-			"moviesposter": "http://a2.mzstatic.com/us/r30/Purple/v4/ac/fb/fb/acfbfb6f-d262-d888-ecfe-d6f4ceb7169c/screen480x480.jpeg",
-			"year": "2008",
-			"description": "Big Buck Bunny (code-named Peach) is a short computer-animated comedy film by the Blender Institute, part of the Blender Foundation. Like the foundation's previous film Elephants Dream, the film was made using Blender, a free software application for animation made by the same foundation. It was released as an open-source film under Creative Commons License Attribution 3.0.",
-			"locname": "mozambique",
-			"location":[
-				{"lat": "-14.234", "lng": "41.504"}
-			],
-			"frames": [
-				{"mainchar": "30-40"},
-				{"outside": "5-10"}
-			]
-		}];
-	return videos;
-}*/
 
 })();
