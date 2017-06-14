2485ba5ce5de9fe4d746a39583d0aeab0ff3d384
Mattia D'Antonio <m.dantonio@cineca.it>
2017-06-14 12:20:17 +0200

    Implementation of video streaming This solution works with a workaround, I fixed the mime type video/mp4 (instead of value read from DB) on both html and API

--- apis/videos.py
+++ apis/videos.py
@@ -3,7 +3,9 @@
 """
 Handle your video metadata
 """
-from flask import stream_with_context, Response
+import os
+import re
+from flask import stream_with_context, Response, request
 from flask import send_file
 from rapydo.confs import get_api_url
 
@@ -238,10 +240,9 @@ class VideoContent(GraphBaseOperations):
                 raise RestApiException(
                     "Video not found",
                     status_code=hcodes.HTTP_BAD_NOTFOUND)
-            f = open(video_uri, "rb")
-            return Response(
-                stream_with_context(self.read_in_chunks(f)),
-                mimetype=item.digital_format[2])
+
+            return self.send_file_partial(video_uri, item.digital_format[2])
+
         elif content_type == 'thumbnail':
             thumbnail_uri = item.thumbnail
             logger.debug("thumbnail content uri: %s" % thumbnail_uri)
@@ -274,3 +275,62 @@ class VideoContent(GraphBaseOperations):
             if not data:
                 break
             yield data
+
+    def send_file_partial(self, path, mime):
+        """
+        Simple wrapper around send_file which handles HTTP 206 Partial Content
+        (byte ranges)
+        TODO: handle all send_file args, mirror send_file's error handling
+        (if it has any)
+        """
+        range_header = request.headers.get('Range', None)
+        if not range_header:
+
+            logger.info("Providing streamed content of %s" % path)
+
+            f = open(path, "rb")
+            return Response(
+                stream_with_context(self.read_in_chunks(f)),
+                mimetype=mime)
+            # return send_file(path)
+
+        size = os.path.getsize(path)
+        byte1, byte2 = 0, None
+
+        m = re.search('(\d+)-(\d*)', range_header)
+        g = m.groups()
+
+        if g[0]:
+            byte1 = int(g[0])
+        if g[1]:
+            byte2 = int(g[1])
+
+        if byte2 is not None:
+            length = byte2 + 1 - byte1
+        else:
+            length = size - byte1
+
+        logger.info(
+            "Providing partial content (bytes %s-%s, len = %s bytes) of %s"
+            % (byte1, byte2, length, path)
+        )
+
+        data = None
+        with open(path, 'rb') as f:
+            f.seek(byte1)
+            data = f.read(length)
+
+        mime = "video/mp4"
+
+        rv = Response(
+            data, hcodes.HTTP_PARTIAL_CONTENT,
+            mimetype=mime,
+            direct_passthrough=True
+        )
+        rv.headers.add(
+            'Content-Range', 'bytes %d-%d/%d'
+            % (byte1, byte1 + length - 1, size)
+        )
+        rv.headers.add('Accept-Ranges', 'bytes')
+
+        return rv
