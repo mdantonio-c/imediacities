e03fa74ece65f2ac8335db0ed39b3065eef957ea
Mattia D'Antonio <m.dantonio@cineca.it>
2017-12-14 08:59:22 +0100

    [#19] Fix backup script

--- scripts/backup_neo4j.sh
+++ scripts/backup_neo4j.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
-timestamp=`date +%s`
-mkdir /tmp/$timestamp
-cd /tmp/$timestamp
-
 docker pause imc_backend_1
 docker pause imc_celery_1
-
 docker stop imc_neo4j_1
 
-docker cp imc_neo4j_1:/data .
+# docker cp imc_neo4j_1:/data .
+
+rsync -avP /home/ubuntu/imediacity/data/graphdata .
 
 docker start imc_neo4j_1
-
 docker unpause imc_backend_1
 docker restart imc_backend_1
-
 docker unpause imc_celery_1
 docker restart imc_celery_1
 
-tar -zcvf data.tar.gz data
+#timestamp=`date +%s`
+#mkdir /tmp/$timestamp
+#tar -zcvf graphdata.tar.gz graphdata
 
-cd -
-mv /tmp/$timestamp/data.tar.gz .
-rm -rf /tmp/$timestamp
-
-
-#rm -rf data
-~                    
