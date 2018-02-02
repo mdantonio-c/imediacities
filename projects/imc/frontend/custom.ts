254d32ca579bc8503adb26f3f49aa28178a6cf97
Mattia <m.dantonio81@gmail.com>
2018-02-02 08:56:44 +0100

    Importing custom stuffs

---
+++ projects/imc/frontend/custom.ts
@@ -0,0 +1,20 @@
+
+import 'ngmap'
+import 'angular-google-chart'
+import 'lodash'
+import 'angular-ui-tree'
+import 'ng-tags-input'
+import 'angular-ivh-treeview'
+// import 'bootstrap-treeview'
+
+import '/app/frontend/css/style.css'
+
+import '/app/frontend/js/data.service.js'
+import '/app/frontend/js/vocabulary.service.js'
+import '/app/frontend/js/codelist.service.js'
+import '/app/frontend/js/admin.archive.controller.js'
+import '/app/frontend/js/search.controller.js'
+import '/app/frontend/js/admin.groups.controller.js'
+import '/app/frontend/js/stage.controller.js'
+import '/app/frontend/js/watch.controller.js'
+import '/app/frontend/js/stage.controller.js'
