8ef0bdcf73df129cb41938c3506e8c5a4c4968e7
Mattia <m.dantonio81@gmail.com>
2018-04-05 14:04:24 +0200

    Fix to load custom brand on navbar

--- projects/imc/frontend/app/app.declarations.ts
+++ projects/imc/frontend/app/app.declarations.ts
@@ -1,6 +1,7 @@
 
 import { CustomNavbarComponent } from './app.custom.navbar';
+import { CustomBrandComponent } from './app.custom.navbar';
 
 export const declarations = [
-	CustomNavbarComponent
+	CustomNavbarComponent, CustomBrandComponent
 ];
