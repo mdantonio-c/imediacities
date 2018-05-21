93cd60bdacfb0add42843abf6d977dd32ab170fa
Mattia <m.dantonio81@gmail.com>
2018-05-21 09:57:12 +0200

    Hardcoded GOOGLE_API_KEY

--- projects/imc/frontend/app/app.imports.ts
+++ projects/imc/frontend/app/app.imports.ts
@@ -1,5 +1,5 @@
 import { NguiMapModule} from '@ngui/map';
 
 export const imports: any[] = [
-  NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=MY_GOOGLE_API_KEY'}),
+  NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyCkSQ5V_EWELQ6UCvVGBwr3LCriTAfXypI'}),
 ];
