f66e6008216e1700d9ee9e71e6158f6e2e8d3785
Mattia <m.dantonio81@gmail.com>
2018-06-04 09:30:12 +0200

    Using sanitize_input implemented in NeomodelClient

--- projects/imc/backend/apis/annotations_search.py
+++ projects/imc/backend/apis/annotations_search.py
@@ -85,7 +85,7 @@ class SearchAnnotations(GraphBaseOperations):
                 if c_match is not None:
                     term = c_match.get('term')
                     if term is not None:
-                        term = self.sanitize_input_term(term)
+                        term = self.graph.sanitize_input(term)
                     multi_match = []
                     multi_match_where = []
                     multi_match_query = ''
@@ -275,10 +275,3 @@ class SearchAnnotations(GraphBaseOperations):
 
         meta_response = {"totalItems": numels}
         return self.force_response(data, meta=meta_response)
-
-    @staticmethod
-    def sanitize_input_term(term):
-        '''
-        Strip and clean up term from special characters.
-        '''
-        return term.strip().replace("*", "").replace("'", "\\'")
--- projects/imc/backend/apis/search.py
+++ projects/imc/backend/apis/search.py
@@ -53,7 +53,7 @@ class Search(GraphBaseOperations):
         if match is not None:
             term = match.get('term')
             if term is not None:
-                term = self.sanitize_input_term(term)
+                term = self.graph.sanitize_input(term)
 
             fields = match.get('fields')
             if term is not None and (fields is None or len(fields) == 0):
@@ -309,10 +309,3 @@ class Search(GraphBaseOperations):
         meta_response["countByYears"] = group_by_years
 
         return self.force_response(data, meta=meta_response)
-
-    @staticmethod
-    def sanitize_input_term(term):
-        '''
-        Strip and clean up term from special characters.
-        '''
-        return term.strip().replace("*", "").replace("'", "\\'")
