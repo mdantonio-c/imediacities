7b6e252c9f8a9c5c334e8046f8de2b76530c6e72
Mattia <m.dantonio81@gmail.com>
2018-07-25 12:28:55 +0200

    Terms of use for registration

--- projects/imc/frontend/app/custom.project.options.ts
+++ projects/imc/frontend/app/custom.project.options.ts
@@ -22,21 +22,73 @@ export class ProjectOptions {
 
 		fields.push(
             {
-                "key": 'research_optin',
-                "type": 'checkbox',
+                "key": 'institution',
+                "type": 'select',
                 "templateOptions": {
-                    "label": 'Accept conditions... '
+                    "label": 'Do you work at one of the following institutions:',
+                    "required": true,
+                    "options": [
+                      { label: 'Archive', value: 'archive' },
+                      { label: 'University', value: 'university' },
+                      { label: 'Research Institution', value: 'research_institution' },
+                      { label: 'None of the above', value: 'none' },
+                    ]
+                }
+            },
+            {
+                "className": "section-label",
+                "template": "<hr><div><strong>To protect your privacy we ask you to accept our:</strong></div><br>"
+            },
+            {
+                "key": 'terms_of_use_optin',
+                "type": 'terms_of_use',
+                "templateOptions": {
+                    "label": 'Terms of Use',
+                    "terms_of_use": require("./terms_of_use.html") 
                 },
                 "validators": {
                     "fieldMatch": {
                         "expression": (control) => control.value,
-                        "message": "You have to accept!"
+                        "message": "This check is mandatory"
+                    }
+                }
+            },
+            {
+                "key": 'privacy_policy_optin',
+                "type": 'terms_of_use',
+                "templateOptions": {
+                    "label": 'Privacy Policy',
+                    "terms_of_use": require("./privacy_policy.html") 
+                },
+                "validators": {
+                    "fieldMatch": {
+                        "expression": (control) => control.value,
+                        "message": "This check is mandatory"
+                    }
+                }
+            },
+            {
+                "key": 'research_optin',
+                "type": 'terms_of_use',
+                "templateOptions": {
+                    "label": 'Research declaration',
+                    "terms_of_use": require("./research_declaration.html")
+                },
+                "validators": {
+                    "fieldMatch": {
+                        "expression": (control) => control.value,
+                        "message": "This check is mandatory"
                     }
                 }
             }
+
         );
 
-        let disclaimer = "Registration disclaimer<br><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</i>"
+        let disclaimer = `
+Welcome to the registration page of I-Media-Cities. Registering a personal account is free of charge, in compliance with European law, and will allow you to enjoy a whole list of additional platform functionalities, such as adding your own information to films and photographs.<br>
+<br>
+It's quick and simple – register now and join a large community all over Europe. 
+`;
 		return {"fields": fields, "disclaimer": disclaimer}
 	}
 
