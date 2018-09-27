6634f1c7be80042fd1f90cf7307847638f2464ae
Cinzia Caroli <c.caroli@cineca.it>
2018-09-27 10:21:36 +0200

    Hide Map view icon in the Catalog page

--- projects/imc/frontend/app/catalog/components/search-navbar/search-navbar.component.ts
+++ projects/imc/frontend/app/catalog/components/search-navbar/search-navbar.component.ts
@@ -1,17 +1,22 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { SearchResultComponent } from "../search-result.component";
 
 @Component({
 	selector: 'search-navbar',
 	templateUrl: './search-navbar.component.html',
 	styleUrls: ['./search-navbar.component.css']
 })
-export class SearchNavbarComponent {
+
+export class SearchNavbarComponent extends SearchResultComponent {
 	@Input() totalItems: number;
 	@Input() loading: boolean;
 	displayMode: string = 'Grid';
 	@Output() onViewChange: EventEmitter<string> = new EventEmitter<string>();
 
+	ngOnChanges() {}
+
 	changeView(choice) {
+		console.log("changeView: user=" + JSON.stringify(this.getUser()));
 		this.displayMode = choice;
 		this.onViewChange.emit(choice);
 	}
