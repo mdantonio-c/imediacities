64190c7870881b9a6a021dbde4d5b882486f6e9e
Mattia <m.dantonio81@gmail.com>
2018-11-24 07:55:05 +0100

    Fix backup and restart scripts to be compatibile with docker-compose 1.23+

--- scripts/backup_neo4j.sh
+++ scripts/backup_neo4j.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-docker pause imc_backend_1
-docker pause imc_celery_1
-docker stop imc_neo4j_1
-
-# docker cp imc_neo4j_1:/data .
+# docker pause $(docker ps | grep imc_backend_ | awk {'print $1'})
+# docker pause $(docker ps | grep imc_celery_ | awk {'print $1'})
+docker stop $(docker ps | grep imc_neo4j_ | awk {'print $1'})
 
 rsync -avP /home/ubuntu/imediacity/data/graphdata .
 
-docker start imc_neo4j_1
-docker unpause imc_backend_1
-docker restart imc_backend_1
-docker unpause imc_celery_1
-docker restart imc_celery_1
+docker start $(docker ps | grep imc_neo4j_ | awk {'print $1'})
+# docker unpause $(docker ps | grep imc_backend_ | awk {'print $1'})
+# docker restart $(docker ps | grep imc_backend_ | awk {'print $1'})
+# docker unpause $(docker ps | grep imc_celery_ | awk {'print $1'})
+# docker restart $(docker ps | grep imc_celery_ | awk {'print $1'})
 
-#timestamp=`date +%s`
-#mkdir /tmp/$timestamp
-#tar -zcvf graphdata.tar.gz graphdata
 
