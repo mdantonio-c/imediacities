5e1e502e2266170f17539b85f0511023d671dfb9
Mattia <m.dantonio81@gmail.com>
2018-11-26 06:39:32 +0100

    Fix backup script for neo4j

--- scripts/backup_neo4j.sh
+++ scripts/backup_neo4j.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# docker pause $(docker ps | grep imc_backend_ | awk {'print $1'})
-# docker pause $(docker ps | grep imc_celery_ | awk {'print $1'})
-docker stop $(docker ps | grep imc_neo4j_ | awk {'print $1'})
+neo4j=$(docker ps | grep imc_neo4j_ | awk {'print $1'})
+# backend=$(docker ps | grep imc_backend_ | awk {'print $1'})
+# celery=$(docker ps | grep imc_celery_ | awk {'print $1'})
+# docker pause $backend
+# docker pause $celery
+docker stop $neo4j
 
 rsync -avP /home/ubuntu/imediacity/data/graphdata .
 
-docker start $(docker ps | grep imc_neo4j_ | awk {'print $1'})
-# docker unpause $(docker ps | grep imc_backend_ | awk {'print $1'})
-# docker restart $(docker ps | grep imc_backend_ | awk {'print $1'})
-# docker unpause $(docker ps | grep imc_celery_ | awk {'print $1'})
-# docker restart $(docker ps | grep imc_celery_ | awk {'print $1'})
+docker start $neo4j
+# docker unpause $backend
+# docker restart $backend
+# docker unpause $celery
+# docker restart $celery
 
 
