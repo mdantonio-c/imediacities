9c88905eae1914d80488796cf2feb61557cb3929
Giuseppe Trotta <g.trotta@cineca.it>
2019-01-11 18:03:21 +0100

    add building mapping (partially completed)

---
+++ projects/imc/backend/tasks/services/building_mapping.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+
+building_mapping = {}
+# ------------------------------------------------------------------
+building_mapping['Athens_Acropolis']                 = {         # 1
+    'iri': 'ChIJ86z1Nxi9oRQR9g3r9ULAl1w',
+    'name': 'Acropolis of Athens',
+    'coord': [37.9715323, 23.7257492]
+}
+building_mapping['Athens_Hilton_Hotel']              = {         # 2
+    'iri': 'ChIJRTnw2US9oRQRkY4AyIMGaZY',
+    'name': 'Hilton Athens',
+    'coord': [37.9765616, 23.7502713]
+}
+building_mapping['Athens_Kallimamarmaro']            = {         # 3
+    'iri': 'ChIJYRK_3mq9oRQRRrCuJb-4Sqc',
+    'name': 'Panathenaic Stadium',
+    'coord': [37.9683342, 23.7411118]
+}
+building_mapping['Athens_National_Library']          = {         # 4
+    'iri': 'ChIJqTF_Izq9oRQRdO7Ezjsto28',
+    'name': 'National Library of Greece',
+    'coord': [37.98111859999999, 23.7330699]
+}
+building_mapping['Athens_Parliament']                = {         # 5
+    'iri': 'ChIJYWGP8zu9oRQRomuwx1zqbss',
+    'name': 'Old Parliament',
+    'coord': [37.9776361, 23.7327983]
+}
+# building_mapping['Athens_Parliament']                = {
+#     'iri': 'ChIJNW8QIz-9oRQRTcr7DV3g7tY',
+#     'name': 'Hellenic Parliament',
+#     'coord': [37.9752774, 23.7369757]
+# }
+building_mapping['Athens_University_of_Athens']      = {         # 6
+    'iri': 'ChIJxRq_LTq9oRQRTdqU4sLcN5Q',
+    'name': 'National and Kapodistrian University of Athens',
+    'coord': [37.968196, 23.7786871]
+}
+# ------------------------------------------------------------------
+building_mapping['Barcelona_Arc_de_Triomf']          = {         # 7
+    'iri': 'ChIJJS1rGuOipBIRVHhfq1KgeTQ',
+    'name': 'Arc de Triomf',
+    'coord': [41.3918835, 2.1805994]
+}
+building_mapping['Barcelona_Casa_Batlio']            = {         # 8
+    'iri': 'ChIJYUFLSe2ipBIRD04uni940kA',
+    'name': 'Casa Batlló',
+    'coord': [41.3916384, 2.1647698]
+}
+building_mapping['Barcelona_Casa_Mila']              = {         # 9
+    'iri': 'ChIJ1eGKmZOipBIRah43T2Kjn8Q',
+    'name': 'Casa Milà',
+    'coord': [41.3953805, 2.1619614]
+}
+building_mapping['Barcelona_Catedral']               = {         # 10
+    'iri': 'ChIJg2xxlvmipBIRg1jhdSQR9hY',
+    'name': 'Cathedral of Barcelona',
+    'coord': [41.383962, 2.1761991]
+}
+building_mapping['Barcelona_Font_Magica']            = {         # 11
+    'iri': 'ChIJN5lVhnCipBIRajHculw3xh4',
+    'name': 'The Magic Fountain',
+    'coord': [41.3711603, 2.1517403]
+}
+building_mapping['Barcelona_Hospital_de_Sant_Pau']   = {         # 12
+    'iri': 'ChIJJ0AB4M-ipBIRj3HSiyLF-gE',
+    'name': 'Hospital de la Santa Creu i Sant Pau',
+    'coord': [41.4136997, 2.174314]
+}
+building_mapping['Barcelona_Monument_a_Colom']       = {         # 13
+    'iri': 'ChIJSwC911aipBIRt2zr380K3Co',
+    'name': 'Mirador de Colom',
+    'coord': [41.3758075, 2.1777689]
+}
+building_mapping['Barcelona_Palau_de']               = {         # 14
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+building_mapping['Barcelona_Palau_de_Musica']        = {         # 15
+    'iri': 'ChIJQQSrd_qipBIRQ2xzarVkqn8',
+    'name': 'Palau de la Música Catalana',
+    'coord': [41.3875752, 2.1753128]
+}
+building_mapping['Barcelona_Palau_Nacional']         = {         # 16
+    'iri': 'ChIJMU59xHGipBIRkdwlcgUWAYY',
+    'name': "Museu Nacional d'Art de Catalunya",
+    'coord': [41.3684399, 2.15357]
+}
+building_mapping['Barcelona_Parlament_de_Catalunya'] = {         # 17
+    'iri': 'ChIJx3A5fwOjpBIRPUlPCmDpgok',
+    'name': 'Parlamento de Cataluña',
+    'coord': [41.3881296, 2.1889826]
+}
+building_mapping['Barcelona_Sagrada_Familia']        = {         # 18
+    'iri': 'ChIJk_s92NyipBIRUMnDG8Kq2Js',
+    'name': 'La Sagrada Familia',
+    'coord': [41.4036299, 2.1743558]
+}
+# ------------------------------------------------------------------
+building_mapping['Brussels_Atomium']                 = {         # 19
+    'iri': 'ChIJAQAAAKzDw0cRCTemaB0Hk1I',
+    'name': 'Atomium',
+    'coord': [50.894941, 4.341546999999999]
+}
+building_mapping['Brussels_Bourse']                  = {         # 20
+    'iri': 'ChIJvQg6oXjEw0cRg29pffSWC6k',
+    'name': 'Brussels Stock Exchange',
+    'coord': [50.8479745, 4.3503697]
+}
+building_mapping['Brussels_Cathedral_St_Goedele']    = {         # 21
+    'iri': 'ChIJvTZ4_3_Dw0cRzSOE0556hYQ',
+    'name': 'St Michael and St Gudula Cathedral, Brussels',
+    'coord': [50.8478332, 4.3601075]
+}
+building_mapping['Brussels_City_Hall']               = {         # 22
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+building_mapping['Brussels_Heizel']                  = {         # 23
+    'iri': 'ChIJN2xMzjbCw0cRlmmkZ5uGHm4',
+    'name': 'Heizel',
+    'coord': [50.8966852, 4.336680600000001]
+}
+building_mapping['Brussels_Jubelpark_Arch']          = {         # 24
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+building_mapping['Brussels_Palais_de_Justice']       = {         # 25
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+building_mapping['Brussels_Royal_Palace']            = {         # 26
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+building_mapping['Brussels_Theatre_Royale']          = {         # 27
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+# ------------------------------------------------------------------
+building_mapping['Frankfurt_Alte_Oper']              = {         # 28
+    'iri': 'ChIJkbtVDq0OvUcRKL3BbW6vzb4',
+    'name': 'Alte Oper',
+    'coord': [50.1160319, 8.671916]
+}
+building_mapping['Frankfurt_Dom']                    = {         # 29
+    'iri': 'ChIJcVvHnKYOvUcRMTFiTc-qNI8',
+    'name': 'Frankfurt Cathedral',
+    'coord': [50.1106631, 8.6854204]
+}
+building_mapping['Frankfurt_Eiserner_Steg']          = {         # 30
+    'iri': 'ChIJox1qlqcOvUcRp60YI90hyz8',
+    'name': 'Iron Bridge',
+    'coord': [50.10809039999999, 8.682131699999999]
+}
+building_mapping['Frankfurt_Eschenheimer_Turm']      = {         # 31
+    'iri': 'ChIJvyk1KrAOvUcR1AOc0r_VDa0',
+    'name': 'Eschenheimer Turm (Restaurant)',
+    'coord': [50.11694689999999, 8.6796919]
+}
+building_mapping['Frankfurt_Grossmarkthalle']        = {         # 32
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+building_mapping['Frankfurt_Hauptbahnhof']           = {         # 33
+    'iri': 'ChIJlSNuYVUJvUcRZmX1t81kEGU',
+    'name': 'Hauptbahnhof',
+    'coord': [50.1076847, 8.6646067]
+}
+building_mapping['Frankfurt_Hauptwache']             = {         # 34
+    'iri': 'ChIJ_9muGq8OvUcRUa-VKCd0_Xs',
+    'name': 'Hauptwache',
+    'coord': [50.1137568, 8.6792611]
+}
+building_mapping['Frankfurt_Paulskirche']            = {         # 35
+    'iri': 'ChIJuYpTG6YOvUcR1NRE7AjKDqg',
+    'name': "St. Paul's Church",
+    'coord': [50.1111941, 8.680757399999999]
+}
+building_mapping['Frankfurt_Roemer']                 = {         # 36
+    'iri': 'ChIJz1QQDqYOvUcRIg-vuOMzHW8',
+    'name': 'Römer',
+    'coord': [50.1104235, 8.6815763]
+}
+# ------------------------------------------------------------------
+building_mapping['Stockholm_Sergels_Torg']           = {         # 37
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+building_mapping['Stockholm_Vaesterbron']            = {         # 38
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+building_mapping['Stockholm_Katarinahissen']         = {         # 39
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+building_mapping['Stockholm_Kungliga_Slottet']       = {         # 40
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+building_mapping['Stockholm_Stadshus']               = {         # 41
+    'iri': '',
+    'name': '',
+    'coord': []
+}
+# ------------------------------------------------------------------
+building_mapping['Turin_Gran_Madre_di_Dio']          = {         # 42
+    'iri': 'ChIJZ1TlXp5yiEcRM1NMuzFMp7g',
+    'name': 'Catholic Parish Church Gran Madre Di Dio',
+    'coord': [45.0623343, 7.699193999999999]
+}
+building_mapping['Turin_Mole_Antonelliana']          = {         # 43
+    'iri': 'ChIJdeBgpXxtiEcRCA1w65O0FjM',
+    'name': 'Mole Antonelliana',
+    'coord': [45.06901130000001, 7.693216199999999]
+}
+building_mapping['Turin_Monte_dei_Cappuccini']       = {         # 44
+    'iri': 'ChIJPdjvk59yiEcRwFr_jW3XZx8',
+    'name': 'Santa Maria del Monte dei Cappuccini',
+    'coord': [45.05953179999999, 7.697408800000001]
+}
+building_mapping['Turin_Palazzo_di_Citta']           = {         # 45
+    'iri': 'ChIJc8Vw6XNtiEcRvancAc7b3Pw',
+    'name': 'Piazza Palazzo di Città',
+    'coord': [45.072855, 7.681507799999999]
+}
+building_mapping['Turin_Palazzo_Madama']             = {         # 46
+    'iri': 'ChIJM5X2P3BtiEcRm_qiGkV47yI',
+    'name': 'Madama Palace',
+    'coord': [45.0709514, 7.685892999999999]
+}
+building_mapping['Turin_Palazzo_Reale']              = {         # 47
+    'iri': 'ChIJC-rXcnBtiEcRjK-icXN-bd8',
+    'name': 'Royal Palace of Turin',
+    'coord': [45.0728337, 7.686331699999999]
+}
+building_mapping['Turin_Porta_Nuova']                = {         # 48
+    'iri': 'ChIJVTl3OWptiEcRoMI0MTAAfZ8',
+    'name': 'Torino Porta Nuova Railway Station',
+    'coord': [45.0624445, 7.678761]
+}
+building_mapping['Turin_Torre_Littoria']             = {         # 49
+    'iri': 'ChIJ9dsjtnFtiEcR_hH52RKPPj4',
+    'name': 'Torre Littoria',
+    'coord': [45.0703035, 7.6838618]
+}
+# ------------------------------------------------------------------
+building_mapping['Vienna_Bundeskanzleramt']          = {         # 50
+    'iri': 'ChIJ8YqXfJcHbUcRx6nypjW-qCM',
+    'name': 'Bundeskanzleramt',
+    'coord': [48.208742, 16.3635852]
+}
+building_mapping['Vienna_Burgtheater']               = {         # 51
+    'iri': 'ChIJzyPFSJYHbUcRXmqp1UY0UTo',
+    'name': 'Burgtheater',
+    'coord': [48.21029619999999, 16.36146]
+}
+building_mapping['Vienna_Neue_Burg']                 = {         # 52
+    'iri': 'ChIJ3wYgv5kHbUcRYE5RLK05Uz4',
+    'name': 'Neue Burg',
+    'coord': [48.205172, 16.3645788]
+}
+building_mapping['Vienna_Parlament']                 = {         # 53
+    'iri': 'ChIJWThkxJYHbUcR0DAnPsJ7GzU',
+    'name': 'Austrian Parliament Building',
+    'coord': [48.20808840000001, 16.3586083]
+}
+building_mapping['Vienna_Rathaus']                   = {         # 54
+    'iri': 'ChIJ_WDSuJUHbUcR2QUpvg7vqR4',
+    'name': 'Rathaus',
+    'coord': [48.2108685, 16.3572486]
+}
+building_mapping['Vienna_Staatsoper']                = {         # 55
+    'iri': 'ChIJ1b8dhpwHbUcRVn03JY0mWf0',
+    'name': 'Vienna Operahouse',
+    'coord': [48.2028769, 16.3689588]
+}
+# ------------------------------------------------------------------
