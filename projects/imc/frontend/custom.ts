e1d6543ae20a5b9e77e472b20b4e5c371c3d5d43
Giuseppe Trotta <g.trotta@cineca.it>
2019-03-04 16:05:22 +0100

    fix problems after upgrading ion-rangeslider

--- projects/imc/frontend/custom.ts
+++ projects/imc/frontend/custom.ts
@@ -8,7 +8,7 @@ import 'slick-carousel/slick/slick.min.js'
 
 // add the ion-range-slider style and skin css
 import 'ion-rangeslider/css/ion.rangeSlider.css'
-// import 'ion-rangeslider/css/ion.rangeSlider.skinFlat.css'
+import 'ion-rangeslider/css/ion.rangeSlider.skinFlat.css'
 
 // Include slick css
 import 'slick-carousel/slick/slick.css'
