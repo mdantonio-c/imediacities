486567a6c64c14fdfcc84853e51cf905c2d0b0a9
Giuseppe Trotta <g.trotta@cineca.it>
2019-03-04 14:14:36 +0100

    solve compiler issue with ngui-map

--- projects/imc/frontend/custom.ts
+++ projects/imc/frontend/custom.ts
@@ -1,12 +1,14 @@
 
 import 'js-marker-clusterer/src/markerclusterer.js'
+
+//$ = jQuery = require('jquery');
 import 'ion-rangeslider/js/ion.rangeSlider.min.js'
 
 import 'slick-carousel/slick/slick.min.js'
 
 // add the ion-range-slider style and skin css
 import 'ion-rangeslider/css/ion.rangeSlider.css'
-import 'ion-rangeslider/css/ion.rangeSlider.skinFlat.css'
+// import 'ion-rangeslider/css/ion.rangeSlider.skinFlat.css'
 
 // Include slick css
 import 'slick-carousel/slick/slick.css'
