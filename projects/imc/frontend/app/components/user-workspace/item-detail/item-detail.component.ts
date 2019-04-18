58c8b23709fbb93b7191257efe0ed535f8e0f929
Giuseppe Trotta <g.trotta@cineca.it>
2019-04-18 17:52:17 +0200

    update virtual gallery URL

--- projects/imc/frontend/app/components/user-workspace/item-detail/item-detail.component.ts
+++ projects/imc/frontend/app/components/user-workspace/item-detail/item-detail.component.ts
@@ -18,7 +18,7 @@ export interface ItemDetail {
 }
 
 // expected https://{url}?list={listID}&access_token={token}
-const VIRTUAL_GALLERY_URL: string = "http://130.186.13.31:8000/imc_vg/start_gallery"
+const VIRTUAL_GALLERY_URL: string = "https://imc-vg.hpc.cineca.it/imc_vg/start_gallery"
 
 @Component({
 	selector: 'item-detail',
