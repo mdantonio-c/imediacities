709b1d8171614f05c2b174f843c225269e8ad814
Mattia <m.dantonio81@gmail.com>
2019-08-02 08:32:26 +0200

    Debug code

--- projects/imc/frontend/app/catalog/components/search-result.component.ts
+++ projects/imc/frontend/app/catalog/components/search-result.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, Injector } from '@angular/core';
 import { Router } from '@angular/router';
 import { MediaUtilsService } from '../services/media-utils.service'
 import { AuthService } from "/rapydo/src/app/services/auth";
@@ -10,14 +10,18 @@ import { AuthService } from "/rapydo/src/app/services/auth";
 export class SearchResultComponent implements OnInit, OnChanges {
 	@Input() media;
 
+	private authService: AuthService;
+
 	identifyingTitle: string;
 	description: string;
 	private user: any;
 
-	constructor(private authService: AuthService, private router: Router) {
+	constructor(private authService: AuthService, private router: Router, private injector: Injector) {
 		console.log("SearchResultComponent.constructor")
 		console.log(authService);
 		console.log(this.authService);
+		console.log(this.injector);
+		this.authService = this.injector.get(AuthService);
 	}
 
 	ngOnInit() {}
