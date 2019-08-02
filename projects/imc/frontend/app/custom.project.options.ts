434f99000e273cb65690e72ceba9049a11f32a38
Mattia <m.dantonio81@gmail.com>
2019-08-02 06:13:27 +0200

    Fix function return typing

--- projects/imc/frontend/app/custom.project.options.ts
+++ projects/imc/frontend/app/custom.project.options.ts
@@ -6,7 +6,7 @@ export class ProjectOptions {
 
 	constructor() {}
 
-	public get_option(opt) {
+	public get_option(opt):any {
 
 		if (opt == 'registration') {
 			return this.registration_options()
