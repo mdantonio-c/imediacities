48a08be0a6763f0cbc82565b6d91bce707a5e6bf
Mattia <m.dantonio81@gmail.com>
2019-12-12 17:18:44 +0100

    Removed old angularjs code

--- projects/imc/frontend/js/constants.js
+++
@@ -1,6 +0,0 @@
-(function() {
-	'use strict';
-
-	angular.module('web')
-		.constant('GOOGLE_API_KEY', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCkSQ5V_EWELQ6UCvVGBwr3LCriTAfXypI&libraries=places');
-})();
