c040f42fb86b8cd4b5ad5ffa496a99ff7d6e1e5d
Mattia <m.dantonio81@gmail.com>
2020-04-23 09:33:49 +0200

    Custom user properties

---
+++ projects/imc/backend/apis/profile.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from marshmallow import fields, validate
+
+
+class CustomProfile(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def manipulate(ref, user, data):
+        data['declared_institution'] = user.declared_institution
+
+        return data
+
+    # strip_required is True when the model is invoked by put endpoints
+    @staticmethod
+    def get_custom_fields(strip_required=False):
+        return {
+            'declared_institution': fields.Str(
+                required=False,
+                default="none",
+                validate=validate.OneOf(
+                    choices=[
+                        'archive',
+                        'university',
+                        'research_institution',
+                        'none',
+                    ],
+                    labels=[
+                        'Archive',
+                        'University',
+                        'Research Institution',
+                        'None of the above',
+                    ]
+                )
+            )
+        }
