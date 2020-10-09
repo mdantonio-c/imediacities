cccd9c72228385df69155c7847c9d7b0a2874112
Giuseppe Trotta <g.trotta@cineca.it>
2020-10-09 19:01:16 +0200

    generate multi language labels in the vocabulary of the controlled terms

--- scripts/convert-vocabulary/converter.py
+++ scripts/convert-vocabulary/converter.py
@@ -1,65 +1,107 @@
-#!.venv/bin/python
-
+#!.venv/bin/python3
 import json
+import sys
 from datetime import datetime
 from functools import reduce
 
 from openpyxl import load_workbook
 
 imc_namespace = "https://imediacities.eu/resource/"
-repls = (" ", "_"), ("/", "_")
+replaces = (" ", "_"), ("/", "_")
 
 
 def generate_iri(term):
-    return imc_namespace + reduce(lambda a, kv: a.replace(*kv), repls, term.strip())
+    return imc_namespace + reduce(lambda a, kv: a.replace(*kv), replaces, term.strip())
 
 
-wb = load_workbook("IMC-Vocabularies-Manual_Annotation-phase-2.xlsx")
-vocabulary = {}
-vocabulary["terms"] = []
-vocabulary["vocabulary"] = "IMC controlled vocabulary"
-vocabulary["created"] = str(datetime.now())
+def lookup_term_class(start_with="Category"):
+    labels = dict()
+    for row in ws.iter_rows(min_row=1, max_col=13, max_row=1):
+        for c in [cell for cell in row if cell.value.startswith(start_with)]:
+            class_label = ws.cell(row=c.row + 1, column=c.column).value
+            if class_label:
+                labels[c.value.rsplit("_", 1)[1].lower()] = class_label.strip()
+    return labels
+
+
+def lookup_iri(idx, header="URI"):
+    for row in ws.iter_rows(min_row=1, max_col=13, max_row=1):
+        for c in [cell for cell in row if cell.value == header]:
+            term_iri = ws.cell(row=idx, column=c.column).value
+            if term_iri:
+                return term_iri.strip()
+
+
+def lookup_labels(idx, start_with, capitalize=False):
+    labels = dict()
+    for row in ws.iter_rows(min_row=1, max_col=13, max_row=1):
+        for c in [cell for cell in row if cell.value.startswith(start_with)]:
+            label = ws.cell(row=idx, column=c.column).value
+            if label:
+                labels[c.value.rsplit("_", 1)[1].lower()] = (
+                    str(label).strip()
+                    if not capitalize
+                    else str(label).strip().capitalize()
+                )
+    return labels
+
+
+input_filename = "IMC-Vocabularies-Manual_Annotation.xlsx"
+wb = None
+try:
+    wb = load_workbook(filename=input_filename)
+except FileNotFoundError:
+    sys.exit(f"ERROR: File '{input_filename}' not found")
+vocabulary = {
+    "terms": [],
+    "vocabulary": "IMC controlled vocabulary",
+    "created": str(datetime.now()),
+}
 for ws in wb.worksheets:
     if ws.sheet_state == "hidden":
         print(f"WARN: ignore hidden sheet: {ws.title}")
         continue
-    # if ws.title != 'Persons(Gender)':
-    #     continue
-    # print('--- {} ---'.format(ws.title))
-    vocabulary_class = {"label": ws.title.lower().capitalize(), "children": []}
 
-    # print('min row {}, max row {}'.format(ws.min_row, ws.max_row))
-    current_group = ws["H2"].value
+    vocabulary_class = {"label": lookup_term_class(), "children": []}
+    # print(json.dumps(vocabulary_class, indent=2, sort_keys=True))
+
     group_terms = {}
     idx_count = 0
     for row_index in range(2, ws.max_row + 1):
         idx_count += 1
-        en_term = ws["L" + str(row_index)].value
-        # de_term = ws['K' + str(row_index)].value
-        if en_term is None:
+        # get List_Entry
+        entry_labels = lookup_labels(row_index, "List_Entry")
+        if not entry_labels:
             # ignore groups without entry value
             continue
-        en_term = en_term.strip()
-        iri = ws["M" + str(row_index)].value
-        if iri is None:
-            iri = generate_iri(en_term)
-        entry = {"id": iri, "label": en_term}
 
-        group = ws["I" + str(row_index)].value
-        if group is not None:
-            g = group.strip().capitalize()
+        # get IRI
+        iri = lookup_iri(row_index)
+        if iri is None:
+            iri = generate_iri(entry_labels["en"])
+        entry = {"id": iri, "label": entry_labels}
+
+        # group = ws["I" + str(row_index)].value
+        group_labels = lookup_labels(row_index, "Subgroup", capitalize=True)
+        if group_labels:
+            g = group_labels.get("en")
+            if not g:
+                sys.exit(
+                    f"ERROR: Invalid input file. Missing 'en' term group for class '{vocabulary_class['label']['en']}'."
+                    f" Current values: {group_labels}"
+                )
             # print("adding entry to group '{}'".format(g))
             actual_terms = group_terms.get(g, None)
             if actual_terms is None:
-                group_terms[g] = [entry]
+                group_terms[g] = (group_labels, [entry])
             else:
-                group_terms[g].append(entry)
+                group_terms[g][1].append(entry)
         else:
             vocabulary_class["children"].append(entry)
 
     for key in group_terms:
         vocabulary_class["children"].append(
-            {"label": key, "children": group_terms[key]}
+            {"label": group_terms[key][0], "children": group_terms[key][1]}
         )
     vocabulary["terms"].append(vocabulary_class)
 
