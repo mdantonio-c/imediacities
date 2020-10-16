0bf3ec7657a214248115359a386b828a14c496d7
Diego Piacentini <diegopiacentini@gmail.com>
2020-10-16 19:23:58 +0200

    in progress

--- projects/imc/frontend/app/catalog/components/map-infowindow/map-infowindow.component.ts
+++ projects/imc/frontend/app/catalog/components/map-infowindow/map-infowindow.component.ts
@@ -3,11 +3,13 @@ import { Component, ElementRef, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'map-infowindow',
   templateUrl: './map-infowindow.component.html',
-  styleUrls: ['./map-infowindow.component.css']
+  styleUrls: ['./map-infowindow.component.css'],
+  providers: []
 })
 export class MapInfowindowComponent {
 
   elRef: ElementRef;
+  marker: any;
 
   constructor(elRef: ElementRef) {
     this.elRef = elRef;
@@ -20,4 +22,5 @@ export class MapInfowindowComponent {
     //This will return '<p> Text </p>' as a string
     return this.elRef.nativeElement.innerHTML;
   }
+  
 }
