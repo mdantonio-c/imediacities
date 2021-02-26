0b51cd72e98db24d1f71c6065221a9fb87e0bd49
Mattia <m.dantonio81@gmail.com>
2021-02-26 15:45:28 +0100

    Removed obsolete files

--- projects/imc/backend/models/sql.py
+++
@@ -1,6 +0,0 @@
-""" CUSTOM Models for the relational database """
-
-from restapi.models.sql import User, db
-
-# Add (inject) attributes to User
-setattr(User, "my_custom_field", db.Column(db.String(255)))
