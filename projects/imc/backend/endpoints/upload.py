811e66f0a1a7303c115df60b0bbea9f48221b3cb
Mattia <m.dantonio@cineca.it>
2021-10-09 08:04:26 +0200

    Replaced custom untyped download with core typed wrapper

--- projects/imc/backend/endpoints/upload.py
+++ projects/imc/backend/endpoints/upload.py
@@ -2,15 +2,14 @@
 Upload a file
 """
 
-import os
 from mimetypes import MimeTypes
 
-from flask import make_response, send_file
 from imc.endpoints import IMCEndpoint
 from restapi import decorators
 from restapi.config import UPLOAD_PATH
 from restapi.connectors import neo4j
-from restapi.exceptions import BadRequest, NotFound
+from restapi.exceptions import BadRequest
+from restapi.services.download import Downloader
 from restapi.services.uploader import Uploader
 from restapi.utilities.logs import log
 
@@ -80,7 +79,7 @@ class Upload(Uploader, IMCEndpoint):
     @decorators.database_transaction
     @decorators.endpoint(
         path="/download/<filename>",
-        summary="Download an uploaded file",
+        summary="Download a file",
         responses={
             200: "File successfully downloaded",
             404: "The uploaded content does not exists",
@@ -102,22 +101,6 @@ class Upload(Uploader, IMCEndpoint):
         if group is None:
             raise BadRequest("No group defined for this user")
 
-        if group is None:
-            raise BadRequest("No group defined for this user")
+        upload_dir = UPLOAD_PATH.joinpath(group.uuid)
 
-        upload_dir = os.path.join("/uploads", group.uuid)
-        if not os.path.exists(upload_dir):
-            os.mkdir(upload_dir)
-            if not os.path.exists(upload_dir):
-                raise NotFound("Upload dir not found")
-
-        staged_file = os.path.join(upload_dir, filename)
-        if not os.path.isfile(staged_file):
-            raise NotFound("File not found. Please specify a valid staged file")
-
-        mime_type = mime.guess_type(filename)
-        log.debug("mime type: {}", mime_type)
-
-        response = make_response(send_file(staged_file))
-        response.headers["Content-Type"] = mime_type[0]
-        return response
+        return Downloader.download(filename, subfolder=upload_dir)
