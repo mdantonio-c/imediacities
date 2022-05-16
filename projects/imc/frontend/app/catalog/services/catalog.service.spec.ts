7d5cc59e49410a723b8ae2e201e00f23f708c4cc
Giuseppe Trotta <g.trotta@cineca.it>
2022-05-16 14:16:34 +0200

    fix testBed for catalog service

--- projects/imc/frontend/app/catalog/services/catalog.service.spec.ts
+++ projects/imc/frontend/app/catalog/services/catalog.service.spec.ts
@@ -1,11 +1,20 @@
 import { TestBed, inject } from "@angular/core/testing";
 
 import { CatalogService } from "./catalog.service";
+import { ApiService } from "@rapydo/services/api";
+import { LocalStorageService } from "./local-storage.service";
+
+class ApiServiceStub {}
+class LocalStorageServiceStub {}
 
 describe("CatalogService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CatalogService],
+      providers: [
+        CatalogService,
+        { provide: ApiService, useClass: ApiServiceStub },
+        { provide: LocalStorageService, useClass: LocalStorageServiceStub },
+      ],
     });
   });
 
